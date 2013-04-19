75c34b580a212fec21b5419e2d9dc9e74df39bd3
luke <luke@68dcebe8-9afe-462d-a69c-c8952e3b1398>
2013-04-19 17:55:13 +0000

    the featureExtractor.py now also looks for common code in a malware family.  The results are very encouraging, but we need to evaluate them more thoroughly to detect bugs and make optimizations before moving forward.
    git-svn-id: https://srl.cajunbot.com/svn/srl/papers/trunk/2013-androidclone/scripts@1980 68dcebe8-9afe-462d-a69c-c8952e3b1398

--- featureExtractor.py
+++ featureExtractor.py
@@ -9,24 +9,27 @@ import os
 
 #create dictionary mapping apks to modules to features
 "for each apk"
-apkDict = {}
+familyDict = {}
+
 for file in os.listdir("data/modules"):
 	apkName = re.search('[^\.]*',file).group(0)
-	apkDict.update({apkName:{}})
-	modData = open("data/modules/"+file).read().strip()
-	modBlobs = modData.split("\n")
+	familyDict.update({apkName:{}})
+	apkData = open("data/modules/"+file).read().strip()
+	modules = apkData.split("\n")
 	
-	modules = []
-	for blob in modBlobs:
-		module = blob.split(",")
-		modules.append(module)
+	for m in range(len(modules)):
+		modules[m] = modules[m].split(",")
+
+	#now modules is a list of modules which are lists of classes
 
 	#for each module in this apk
 	for i in range(len(modules)):
+		moduleNum = str(i)
 		featureSet = set()
-		outputName = apkName+"."+str(i)+".features"
+		outputName = apkName+"."+moduleNum+".features"
+		classList = modules[i]
 		#for each class in the module
-		for c in modules[i]:
+		for c in classList:
 			className = c.replace("/",".")
 			prefix = "data/flatClasses/"+apkName+".classes/"
 			classData = open(prefix+className).read()
@@ -34,23 +37,25 @@ for file in os.listdir("data/modules"):
 			#the weird symbols at the end of the line shouldn't hurt anything
 			apiCalls = re.findall(', Landroid/.*;->.*',classData)
 			apiCalls = [x.replace(", L","") for x in apiCalls]
-			#apiCalls = [x.replace(";->","") for x in apiCalls]
 			apiCalls = set(apiCalls)
 			#start unioning the results of patternmatching
 			featureSet = featureSet.union(apiCalls)
 			if len(featureSet) > 0:
-				apkDict[apkName].update({str(i):featureSet})
-		
+				#TODO tieBreaker of module is 0 for now.  Make a function to calculate this to break ties in similarity
+				moduleDict={"classList":classList,"featureSet":featureSet,"tieBreaker":0}
+				familyDict[apkName].update({moduleNum:moduleDict})
+
 #figure out how many features there are
 allFeatures = set()
 modList = []
-for apk in apkDict:
-	for module in apkDict[apk]:
-		allFeatures = allFeatures.union(apkDict[apk][module])
+for apk in familyDict:
+	for module in familyDict[apk]:
+		allFeatures = allFeatures.union(familyDict[apk][module]["featureSet"])
 		modList.append((apk,module))
 allFeatures = list(allFeatures)
 
 #output header
+"""
 header = "apkName,moduleNumber,"
 for f in range(len(allFeatures)):
 	header = header + allFeatures[f] + ","
@@ -60,13 +65,57 @@ print header[:-1]
 for mod in modList:
 	fVector = mod[0]+','+mod[1]+','
 	for feature in allFeatures:
-		if feature in apkDict[mod[0]][mod[1]]:
+		if feature in familyDict[mod[0]][mod[1]]:
 			fVector = fVector + "1,"
 		else:
 			fVector = fVector + "0,"
 	print fVector[:-1]
+"""
+
+#Now that I've got the familyDictionary how do I best use it to find shared modules?
+# the next line can be used to get the key mapping to the highest score
+# key seems to be an option of max.  This takes a list of keys and evaluates them 
+#for max by the values they map to instead of the keys themselves.  This could also do sorting.
+#max(scoreTable.keys(), key=(lambda x: scoreTable[x]))
+#I also need to detect and handle ties...
+#sorted(scoreTable.items(), key=(lambda (k,v): v))
+#the parameters to lambda represent each element of the list like in a map.
+
+#we only want to run comparisons with the first apk as the source.  The others can be targets.
 
 
+moduleToSimilarityScore = {}
+#I need to compare one apk to the rest so I'm going to pop.  This will remove the popped element from familyDict
+(chosenApkName,chosenApkDict) = familyDict.popitem()
 
-			
+def calcSimilarity(set1,set2):
+	cardIntersect = len(set1.intersection(set2))
+	cardUnion = len(set1.union(set2))
+	return float(cardIntersect) / float(cardUnion)
+
+for (chosenModuleKey,chosenModuleDict) in chosenApkDict.items():
+	chosenFeatureSet=chosenModuleDict["featureSet"]
+	moduleToSimilarityScore.update({chosenModuleKey:0})
+	for apkDict in familyDict.values():
+		#compare the chosenFeature set to all the featureSets in each module of this apk and get the highest score found.
+		featureSetList = map ((lambda moduleDict: moduleDict["featureSet"]), apkDict.values()) 
+		maxSimilarityScore = max(map ((lambda featureSet: calcSimilarity(chosenFeatureSet,featureSet)), featureSetList))
+		moduleToSimilarityScore[chosenModuleKey] += maxSimilarityScore
+		#print chosenModuleKey, moduleToSimilarityScore[chosenModuleKey]
+	moduleToSimilarityScore[chosenModuleKey] /= len(familyDict)
+
+#this sorted output is for testing.  I actually want max for the real system.
+#how should I handle ties?  Should I combine them into one larger module?  That could interfere when comparing to new modules.
+#keep both and search for either?
+sortedScores = sorted(moduleToSimilarityScore.items(), key=(lambda (k,v): -v))
+print chosenApkName
+for score in sortedScores:
+	print score
+	repModule = chosenApkDict[score[0]]
+	print repModule["classList"]
+	print repModule["featureSet"]
+
+
+#TODO I will need a way to go from module number to outputting which classes are in the module.  Another dictionary should work.
+
 			
